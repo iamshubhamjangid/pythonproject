92b4cb9e61dfa2cef0c7c93964adec9b0697bf0b
iamshubhamjangid <35128560+iamshubhamjangid@users.noreply.github.com>
2018-02-12 07:33:25 +0530

    Add files via upload

--- voter.py
+++ voter.py
@@ -1,7 +1,7 @@
 class vdetails:
     def voterinit(self):
-	v_name=raw_input("Enter Voter Name here \t")
-	v_age=int(input("Enter Voter age here\t"))
-	v_id=raw_input("Enter VOter Unique Voter id here\t")
-	v_aadhar=raw_input("Enter your aadhar Number here\t")
-
+        v_name = input("Enter Voter Name here \t")
+        v_age = int(input("Enter Voter age here\t"))
+        v_id = input("Enter VOter Unique Voter id here\t")
+        v_aadhar = input("Enter your aadhar Number here\t")
+        return v_id, v_aadhar, v_age, v_name
--- voterport.py
+++ voterport.py
@@ -1,26 +1,25 @@
-from voter import voter
+from voter import vdetails
 class VoterManager:
-    vfile=open("vtfile.txt",'a')
+    vfile = open("vtfile.txt", 'a')
     def voteradd(self):
-          vfile=open("vtfile.txt",'a')
-          vfile.write(voter.v_id)
-          vfile.write(":")
-          vfile.write(voter.v_name)
-          vfile.write("\n")
-          print(vfile)
+        vfile = open("vtfile.txt", 'a')
+        vfile.write(vdetails.v_id)
+        vfile.write(":")
+        vfile.write(vdetails.v_name)
+        vfile.write("\n")
+        return vfile
     def isvalidvoter(self):
-          vt=voter()
-          if vt.v_age>=18:
-            vfile=open("vtfile.txt",'r')
-            vdict={}
-            for line in vfile:
-              x=line.split(":")
-              vt_id=x[0]
-              vt_name=x[1]
-              vdict[vt_id]=vt_name
-            if vdict.has_key(vt.v_id):
-              print("User already exist!!!! \nNot allowed to vote!!")
-            else:
-              vtm=VoterManager().voteradd()
-	        else:
+         if vdetails.voterinit(self).v_age >= 18:
+                vfile = open("vtfile.txt",'r')
+                vdict = {}
+                for line in vfile:
+                    x = line.split(":")
+                    vt_id = x[0]
+                    vt_name = x[1]
+                    vdict[vt_id] = vt_name
+                if vdetails.voterinit().v_id in vdict :
+                    print("User already exist!!!! \nNot allowed to vote!!")
+                else:
+                     vtm = VoterManager().voteradd
+         else:
             print("not even eligible for voting")
