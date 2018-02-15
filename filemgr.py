65fdc636e8f7661bd9e807e3bd2dd67d5744901b
iamshubhamjangid <35128560+iamshubhamjangid@users.noreply.github.com>
2018-02-15 19:04:33 +0530

    Add files via upload

--- filemgr.py
+++ filemgr.py
@@ -2,16 +2,31 @@ from datetime import date
 from vtrmgr import votermgr
 from cdmgr import candidatemgr	
 class filemanager:
-    def addvoter(self,name,vid):
-	votermgr.addvoter(self,name,vid)
-    def deletevoter(self,vid):
-	votermgr.deletevoter(self,vid)
-    def displayvoters(self):
-	votermgr.displayvoter(self)
+   def AddVoter():
+       
 
-    def addcandidate(self,name,cid):
-	candidatemgr.addcandidate(self,name,cid)
-    def deletecandidate(self,cid):
-	candidatemgr.deletecandidate(self,cid)
-    def displaycandidates(self):
-	candidatemgr.displaycandidate(self)
+   def AddCandidate():
+   
+   def DeleteVoter():
+    print "n is an even number\n"
+
+   def DeleteCandidate():
+    print "n is a prime number\n"
+   
+   def ListVoters():
+    print "n is a prime number\n"
+
+   def ListCandidates():
+    print "n is a prime number\n"
+
+# map the inputs to the function blocks
+   options = {0 : 
+zero,
+           1 : sqr,
+           4 : sqr,
+           9 : sqr,
+           2 : even,
+           3 : prime,
+           5 : prime,
+           7 : prime,
+}
